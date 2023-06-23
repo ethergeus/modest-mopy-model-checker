549e54ac6a1a24d234eb01ae9e4ac4887bcfa8d5
Andrey Antonowycz (thawer) <andrey@antonowycz.com>
2023-06-23 16:11:26 +0200

    Add experiments

--- experiments.sh
+++ experiments.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# ./model-checker.py modelPy/single-transition.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note ordinal
+# ./model-checker.py modelPy/single-transition.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --note ordinal
 # ./model-checker.py modelPy/single-transition.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note onehot
 
-# ./model-checker.py modelPy/success-fail.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note ordinal
+# ./model-checker.py modelPy/success-fail.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 ---note ordinal
 # ./model-checker.py modelPy/success-fail.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note onehot
 
-# ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note ordinal
+# ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --note ordinal
 # ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R1 --onehot-all --note onehot
-# ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R2 --onehot-all --note ordinal
+# ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R2 --note ordinal
 # ./model-checker.py modelPy/safe-risk.py --deep-q-learning -v --plot --alpha 1e-4 -k 1000 --properties R2 --onehot-all --note onehot
+
+./model-checker.py modelPy/eajs.py --deep-q-learning -v --plot --alpha 1e-4 -k 500 --properties ExpUtil --onehot-all --batch-size 512 --note onehot-batch512
+./model-checker.py modelPy/eajs.py --deep-q-learning -v --plot --alpha 1e-4 -k 500 --properties ExpUtil --onehot-all --batch-size 512 --double-q --note onehot-double-batch512
 
 exit 0
